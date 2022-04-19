efd8e20ee327d2cdc083c3f308c61e96cfe8a473
Jordan Cates <62562361+helloimjordan@users.noreply.github.com>
2022-04-19 08:50:55 -0700

    edit comments

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
+"""
+AppConfig subclass to be loaded automatically in INSTALLED_APPS
+https://docs.djangoproject.com/en/3.2/ref/applications/#configuring-applications
+"""
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
